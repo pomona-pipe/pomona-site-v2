d80874f32fd8f8d9e1cfb5c9ed3f090e4b51ff5b
Dillon Joyce <dmjoyce92@gmail.com>
2020-07-23 10:26:25 -0400

    Extract mobile menu from navigation drawer

--- components/Navigation/MobileDrawer.vue
+++ components/Navigation/MobileDrawer.vue
@@ -11,51 +11,7 @@
       disable-resize-watcher
       class="pb-16"
     >
-      <v-list nav dense>
-        <v-list-item-group>
-          <v-list-item
-            two-line
-            to="/"
-            active-class="deep-purple--text text--accent-4"
-          >
-            <v-list-item-icon class="mr-4 align-self-center">
-              <v-icon x-large>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>Home</v-list-item-content>
-          </v-list-item>
-        </v-list-item-group>
-        <v-list-group
-          v-for="navOption in mainNavigation"
-          :key="navOption.primary.link.id"
-          active-class="deep-purple--text text--accent-4"
-        >
-          <!-- Dropdown title -->
-          <template v-slot:activator>
-            <v-list-item two-line :nuxt="true">{{
-              navOption.primary.label[0].text
-            }}</v-list-item>
-          </template>
-          <!-- Sub Nav Options -->
-          <v-list-item
-            v-for="subNavOption in navOption.items"
-            :key="subNavOption.sub_nav_link.id"
-            :nuxt="true"
-            :to="{
-              path:
-                navOption.primary.link.uid === subNavOption.sub_nav_link.uid
-                  ? `/${navOption.primary.link.uid}`
-                  : `/${navOption.primary.link.uid}/${subNavOption.sub_nav_link.uid}`
-            }"
-            active-class="deep-purple--text text--accent-4"
-            two-line
-            exact
-          >
-            <v-list-item-subtitle>{{
-              subNavOption.sub_nav_link_label[0].text
-            }}</v-list-item-subtitle>
-          </v-list-item>
-        </v-list-group>
-      </v-list>
+      <MobileMenu />
     </v-navigation-drawer>
   </div>
 </template>
@@ -64,11 +20,13 @@
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
-import { mapState } from 'vuex'
+import MobileMenu from '~/components/Navigation/MobileMenu.vue'
 
 @Component({
+  components: {
+    MobileMenu
+  },
   computed: {
-    ...mapState('layout', ['mainNavigation']),
     mobileDrawer: {
       get() {
         return this.$store.state.layout.mobileDrawer
