97ac65245abfb995df9da5473c8c1517fc61fbf8
BENZABOSS <joyceben33@gmail.com>
2020-07-24 11:36:22 -0400

    Design tweaks: font-color, hero-image position, etc.

--- pages/about-us/index.vue
+++ pages/about-us/index.vue
@@ -1,37 +1,45 @@
 <template>
   <v-row cols="12">
-    <v-col sm="12">
-      <h1 align="center">{{ aboutUs[0].data.main_title[0].text }}</h1>
-    </v-col>
-    <v-col sm="12">
+    <v-col sm="12" class="px-0 py-0">
       <v-img
         :src="aboutUs[0].data.main_image.url"
         gradient="to top right, rgba(36, 36, 36, 0.9), rgba(25,32,72,.7)"
-        max-height="336px"
+        max-height="250px"
       >
-        <v-row align="center" class="lightbox white--text pa-2 fill-height">
+        <v-row align="center" class="fill-height">
           <v-col align="center">
-            <h1>{{ aboutUs[0].data.main_image.alt }}</h1>
+            <h2 class="subheader">{{ aboutUs[0].data.main_title[0].text }}</h2>
+            <h1 class="header">{{ aboutUs[0].data.main_image.alt }}</h1>
           </v-col>
         </v-row>
       </v-img>
     </v-col>
-    <v-col>
-      <v-row cols="12">
-        <v-col sm="12">
-          <h2>{{ aboutUs[0].data.summary_title[0].text }}</h2>
+    <v-col class="center align column">
+      <v-row cols="12" justify="center" >
+        <v-col sm="12" align="center">
+          <h1>{{ aboutUs[0].data.summary_title[0].text }}</h1>
         </v-col>
         <v-col sm="12">
-          <p>{{ aboutUs[0].data.company_summary[0].text }}</p>
+          <p class="paragraph">{{ aboutUs[0].data.company_summary[0].text }}</p>
         </v-col>
       </v-row>
     </v-col>
   </v-row>
 </template>
 <style lang="css" scoped>
-.titleCenter {
-  text-align: center;
+.subheader {
+  color: #cccccc;
 }
+.header {
+  color: #ffffff
+}
+.paragraph {
+  color: #666666;
+  padding-left: 10%;
+  padding-right: 10%;
+  font-size: x-large;
+}
+
 </style>
 
 <script lang="ts">
