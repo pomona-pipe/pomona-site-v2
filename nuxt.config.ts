e57362636e5cc979ecb67594cba33026ed55648c
Dillon Joyce <dmjoyce92@gmail.com>
2020-07-28 14:22:36 -0400

    Converted vuetify plugin to ts, added themes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,4 @@
 import { Configuration } from 'webpack'
-import colors from 'vuetify/es5/util/colors'
 
 export default {
   mode: 'universal',
@@ -31,7 +30,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: ['@plugins/vuetify'],
+  plugins: ['@plugins/vuetify.ts'],
   /*
    ** Nuxt.js dev-modules
    */
@@ -62,21 +61,7 @@ export default {
    ** https://github.com/nuxt-community/vuetify-module
    */
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
-    theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
+    customVariables: ['~/assets/variables.scss']
   },
   /*
    ** Build configuration
