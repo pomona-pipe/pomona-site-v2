3f0e13ac29275c65a641b03c0bb5842851574fd9
Dillon Joyce <dmjoyce92@gmail.com>
2020-08-02 21:38:08 -0400

    Correct link resolver typing and handle product links asynchronously

--- plugins/link-resolver.ts
+++ plugins/link-resolver.ts
@@ -5,8 +5,17 @@
 
 import { Document } from 'prismic-javascript/d.ts/documents'
 
-interface IPrismicDocument extends Document {
-  isBroken?: boolean
+// from https://prismic.io/docs/javascript/beyond-the-api/link-resolving
+interface DocumentLink {
+  id: string
+  isBroken: boolean
+  lang: string
+  // eslint-disable-next-line camelcase
+  link_type: string
+  slug: string
+  tags: string[]
+  type: string
+  uid: string
 }
 
 function parseUidFromName(uid: string) {
@@ -25,19 +34,39 @@ function parseUidFromName(uid: string) {
   return words.join('-')
 }
 
-export default function(doc: IPrismicDocument) {
+export default function(doc: DocumentLink) {
   if (doc.isBroken) {
     return '/not-found'
   }
 
-  const { uid, data } = doc
+  const { uid } = doc
 
   switch (doc.type) {
     case 'product_categories':
       return `/products/${uid}`
 
     case 'products':
-      return `/products/${parseUidFromName(data.product_category)}/${uid}`
+      if (process.client) {
+        // overwrite initial href value once product category is retrieved
+        ;(window.$nuxt as any).$prismic.api
+          .getByUID('products', uid)
+          .then((result: Document) => {
+            const productLink = `/products/${parseUidFromName(
+              result!.data.product_category
+            )}/${uid}`
+            const productAnchorTags = document.querySelectorAll(
+              'a[href*="product_category"]'
+            )
+            productAnchorTags.forEach((el, key) => {
+              if (el.getAttribute('href')!.includes(uid)) {
+                productAnchorTags[key].setAttribute('href', productLink)
+              }
+            })
+          })
+          .catch(() => '/not-found')
+      }
+      // initial href value - overwritten after api call
+      return `/products/product_category/${uid}`
 
     case 'projects':
       return `/projects/${uid}`
