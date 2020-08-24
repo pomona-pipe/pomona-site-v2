e9ce583fab883d9efa5dd4e5dbdc6cbb84b7fa4c
Dillon Joyce <dmjoyce92@gmail.com>
2020-08-24 15:13:32 -0400

    Testing serverless functions

---
+++ functions/test.ts
@@ -0,0 +1,18 @@
+/* eslint-disable require-await */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Handler } from 'aws-lambda'
+
+const handler: Handler = async (event, context) => {
+  if (event.httpMethod !== 'GET') {
+    return { statusCode: 405, body: 'Method Not Allowed' }
+  }
+
+  const name = event.queryStringParameters.name || 'World'
+
+  return {
+    statusCode: 200,
+    body: `Hello, ${name}`
+  }
+}
+
+exports.handler = handler
