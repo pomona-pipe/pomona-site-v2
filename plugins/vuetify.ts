386b6336ee02093d5ca0a2ba5390926d5bc90ed3
Dillon Joyce <dmjoyce92@gmail.com>
2020-08-26 08:53:33 -0400

    Remove css icon import

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import { Context } from '@nuxt/types'
 import theme from '~/settings/theme'
-import '@mdi/font/css/materialdesignicons.css'
 Vue.use(Vuetify)
 
 export default (ctx: Context) => {
