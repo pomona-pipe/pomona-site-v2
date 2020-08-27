c31acaf0c579efaea9c3925be539f8f6a417692c
Dillon Joyce <dmjoyce92@gmail.com>
2020-08-27 07:56:20 -0400

    Vertically center customer logos

--- components/PageComponents/HomePageSections/CustomerLogo.vue
+++ components/PageComponents/HomePageSections/CustomerLogo.vue
@@ -2,22 +2,32 @@
   <section>
     <!-- Customer Logos Section -->
     <v-container>
-      <prismic-rich-text class="text-center" :field="home[0].data.customer_logo_title" />
-      <v-row class="justify-space-around">
-        <v-col class="flex-grow-0" v-for="logo in home[0].data.customer_logos" :key="logo.id">
-          <v-img :src="logo.image.url" max-height="200px" max-width="150px"></v-img>
+      <prismic-rich-text
+        class="text-center"
+        :field="home[0].data.customer_logo_title"
+      />
+      <v-row class="justify-space-around align-center">
+        <v-col
+          v-for="logo in home[0].data.customer_logos"
+          :key="logo.id"
+          class="flex-grow-0"
+        >
+          <v-img
+            :src="logo.image.url"
+            max-height="200px"
+            max-width="150px"
+          ></v-img>
         </v-col>
       </v-row>
     </v-container>
   </section>
 </template>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
-import { Store, mapState } from 'vuex'
+import { mapState } from 'vuex'
 
 @Component({
   components: {},
