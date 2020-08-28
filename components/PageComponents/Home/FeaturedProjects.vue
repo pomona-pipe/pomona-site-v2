97381c23ba336c53bd5c7a818a119f355e94a0c4
BENZABOSS <joyceben33@gmail.com>
2020-08-28 17:13:34 -0400

    updated for change in data stucture on projects in store

--- components/PageComponents/Home/FeaturedProjects.vue
+++ components/PageComponents/Home/FeaturedProjects.vue
@@ -22,7 +22,7 @@
             >
               <v-img
                 :src="
-                  project.data.project_image.listing_page.url ||
+                  project.data.project_description_hero.url ||
                     placeholders.file
                 "
               ></v-img>
@@ -31,11 +31,11 @@
                 project.data.project_name[0].text
               }}</v-card-title>
               <v-card-text class="text--primary">{{
-                project.data.project_description[0].text
+                project.data.project_description
               }}</v-card-text>
               <v-card-subtitle>
-                {{ formatDateString(project.data.completion_date) }} in
-                {{ project.data.project_location[0].text }}
+                {{ formatDateString(project.data.overview_completion_date) }} in
+                {{ project.data.project_location }}
               </v-card-subtitle>
             </v-card>
           </v-hover>
