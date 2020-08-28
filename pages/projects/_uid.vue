8d9e05608a85248151d0393fdf8a903f2847e603
BENZABOSS <joyceben33@gmail.com>
2020-08-28 17:15:19 -0400

    Segmenting project detail page into components

--- pages/projects/_uid.vue
+++ pages/projects/_uid.vue
@@ -1,25 +1,7 @@
 <template>
-  <section>
-    <v-row cols="12">
-      <v-col sm="6">
-        <v-img
-          :src="
-            document.data.project_image.detail_page.url || placeholders.file
-          "
-        ></v-img>
-      </v-col>
-      <v-col sm="6">
-        <h1 align="center">{{ document.data.project_name[0].text }}</h1>
-        <h4>
-          {{ formatDateString(document.data.completion_date) }} in
-          {{ document.data.project_location[0].text }}
-        </h4>
-        <div v-for="element in document.data.project_summary" :key="element.id">
-          <p>{{ element.text }}</p>
-        </div>
-      </v-col>
-    </v-row>
-  </section>
+  <div class="page">
+    <ProjectDescription />
+  </div>
 </template>
 
 <script lang="ts">
@@ -27,19 +9,15 @@ import { Component, Vue } from 'nuxt-property-decorator'
 import { Store, mapState } from 'vuex'
 import { Route } from 'vue-router/types'
 import { find } from 'lodash'
-import moment from 'moment'
 import { IPrismic, IPrismicDocument } from '~/shims'
+import ProjectDescription from '~/components/PageComponents/ProjectDetailSections/ProjectDescription.vue'
 @Component({
-  computed: {
-    ...mapState('layout', ['placeholders'])
-  }
+  components: {
+    ProjectDescription
+  },
+  
 })
 export default class DetailPage extends Vue {
-  document: IPrismicDocument | null = null
-
-  formatDateString(dateString: string) {
-    return moment(dateString).format('MMMM Do YYYY')
-  }
 
   async fetch({
     store,
@@ -61,10 +39,5 @@ export default class DetailPage extends Vue {
     store.commit('projects/addProject', result)
   }
 
-  // fetch project from store and copy to component
-  created() {
-    const uid = this.$route.params.uid
-    this.document = find(this.$store.state.projects.projects, { uid })
-  }
 }
 </script>
