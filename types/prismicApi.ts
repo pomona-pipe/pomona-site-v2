b86123c4729286abfe4a16847cd1f131a9f7200a
dmjoyce92 <dmjoyce92@gmail.com>
2020-09-08 11:29:49 -0400

    Additonal prismic api types

--- types/prismicApi.ts
+++ types/prismicApi.ts
@@ -1,3 +1,18 @@
+/* eslint-disable camelcase */
+interface PrismicResult {
+  id: string
+  title: string
+  description: string
+  image_url: string
+  last_update: string
+  blob: Blob
+}
+
+interface PrismicResonseBody {
+  results_size: number
+  results: PrismicResult[]
+}
+
 interface PrismicResponse {
   statusCode: number
   body?: string
