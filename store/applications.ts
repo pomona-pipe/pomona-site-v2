88b4fc928ca39f80ea1e5ad7fa3a19b13f8d0d81
BENZABOSS <joyceben33@gmail.com>
2020-09-08 16:59:39 -0400

    setup store for repeatble applications type.

---
+++ store/applications.ts
@@ -0,0 +1,33 @@
+import { uniqBy } from 'lodash'
+
+// TODO: create types for api response data/payloads
+import { IPrismic } from '~/shims'
+
+interface IState {
+  applications: object[]
+}
+
+export const state = () => ({
+  applications: []
+})
+
+export const mutations = {
+  setApplications(state: IState, payload: object[]) {
+    state.applications = payload
+  }
+}
+export const actions = {
+  async getApplications({ commit }: { commit: any }, $prismic: IPrismic) {
+    const byApplications = $prismic.predicates.at(
+      'document.type',
+      'applications'
+    )
+    const applications = await $prismic.api.query(byApplications, {
+      orderings: '[my.applications.order_number]'
+    })
+    commit(
+      'setApplications',
+      applications.results.map((result) => result)
+    )
+  }
+}
