5788b78c516b9e74cb4af7e5e92211c4ca7b68ac
Dillon Joyce <dmjoyce92@gmail.com>
2020-09-15 11:22:58 -0400

    Remove listen server

--- api/index.ts
+++ api/index.ts
@@ -9,12 +9,3 @@ app.use(dropbox)
 
 // Export express app
 export default app
-
-// Start standalone server if directly running
-if (process.env.NODE_ENV === 'production') {
-  const port = process.env.PORT || 3001
-  app.listen(port, () => {
-    // eslint-disable-next-line no-console
-    console.log(`API server listening on port ${port}`)
-  })
-}
