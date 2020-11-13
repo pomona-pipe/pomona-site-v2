980628ea3435f2a033346f150fdb3c24a70ec596
Dillon Joyce <dmjoyce92@gmail.com>
2020-11-13 11:24:02 -0500

    Fix padding

--- components/Navigation/SearchBar.vue
+++ components/Navigation/SearchBar.vue
@@ -21,6 +21,7 @@
             :class="{
               'pl-4': true,
               'pr-13': true,
+              'py-2': true,
               'slide-in': true,
               in: searchBar.open && !searchBar.isClosing
             }"
@@ -89,7 +90,6 @@
     width: 100%;
     left: 0;
     top: 0;
-    padding: 8px 28px;
     z-index: 1;
     background-color: #ffffff;
   }
