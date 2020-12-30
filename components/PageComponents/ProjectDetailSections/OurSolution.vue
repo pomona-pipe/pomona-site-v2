413071e5a66a3b9b4c94d08dfff0fe79791bd3f3
BENZABOSS <joyceben33@gmail.com>
2020-12-30 15:20:48 -0500

    Project Detail page layout update.

--- components/PageComponents/ProjectDetailSections/OurSolution.vue
+++ components/PageComponents/ProjectDetailSections/OurSolution.vue
@@ -1,21 +1,21 @@
 <template>
   <section class="page">
-    <v-container>
-      <v-row>
-        <v-col cols="12" md="6" order-md="2">
-          <h2>Our Solution</h2>
-          <prismic-rich-text :field="document.data.our_solution_paragraph" />
-        </v-col>
-        <v-col cols="12" md="6" order-md="1">
-          <v-img :src="document.data.our_solution_image.fileUrl"></v-img>
-        </v-col>
-      </v-row>
-    </v-container>
+    <v-row class="mx-0">
+      <v-col class="px-0" cols="11" md="5">
+        <v-img
+          class="rounded-r-xl"
+          :src="document.data.our_solution_image.fileUrl"
+        ></v-img>
+      </v-col>
+      <v-col class="pl-0" cols="10" offset="1" md="5">
+        <h2>Our Solution</h2>
+        <prismic-rich-text :field="document.data.our_solution_paragraph" />
+      </v-col>
+    </v-row>
   </section>
 </template>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
