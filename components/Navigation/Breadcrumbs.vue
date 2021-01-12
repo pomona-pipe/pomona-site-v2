c6ed4795d8240d66055c37d6cfa891f6b52c4924
BENZABOSS <joyceben33@gmail.com>
2021-01-12 10:36:32 -0500

    created breadcrumbs component

---
+++ components/Navigation/Breadcrumbs.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <v-breadcrumbs dark :items="breadcrumbs">
+      <template v-slot:divider>
+        <v-icon small>{{ mdiChevronRight }}</v-icon>
+      </template>
+    </v-breadcrumbs>
+  </div>
+</template>
+
+<script lang="ts">
+import { Prop, Component, Vue } from 'nuxt-property-decorator'
+import { mdiChevronRight } from '@mdi/js'
+
+@Component({})
+export default class Breadcrumb extends Vue {
+  mdiChevronRight = mdiChevronRight
+
+  @Prop() breadcrumbs!: IBreadcrumb[]
+}
+</script>
